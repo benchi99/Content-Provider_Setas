596b0cf8e2d6b1bf25f8f999e89a0d3187eaa772
Rubén (benchi99) Bermejo <mejoruben@gmail.com>
2019-02-02 09:56:59 +0100

    Initial commit

---
+++ app/src/main/java/com/rubenbermejo/fml/contentprovidersetas/ObjetoSetas.java
@@ -0,0 +1,90 @@
+package com.rubenbermejo.fml.contentprovidersetas;
+
+public class ObjetoSetas {
+
+    private String nombre, descripcion, nombreComun, URLlinea;
+    private boolean Comestible, favorito;
+    private int id;
+    private byte[] imagen;
+
+    public ObjetoSetas(String nombre, String descripcion, String nombreComun, String URLlinea, boolean Comestible, byte[] imagen) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.nombreComun = nombreComun;
+        this.URLlinea = URLlinea;
+        this.Comestible = Comestible;
+        this.imagen = imagen;
+        this.favorito = false;
+    }
+
+    public int getId() { return id; }
+
+    public void setId(int id) { this.id = id; }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getnombreComun() {
+        return nombreComun;
+    }
+
+    public void setnombreComun(String nombreComun) {
+        this.nombreComun = nombreComun;
+    }
+
+    public String getURLlinea() {
+        return URLlinea;
+    }
+
+    public void setURLlinea(String URLlinea) {
+        this.URLlinea = URLlinea;
+    }
+
+    public boolean getComestible() {
+        return Comestible;
+    }
+
+    public void setComestible(boolean Comestible) {
+        this.Comestible = Comestible;
+    }
+
+    public byte[] getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(byte[] imagen) {
+        this.imagen = imagen;
+    }
+
+    public boolean getFavorito() { return favorito; }
+
+    public void setFavorito(boolean favorito) { this.favorito = favorito; }
+
+    private String getToStringComestible(){
+
+        if (getComestible()) {
+            return "comestible";
+        } else {
+            return "no comestible";
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        return "¿Sabías que la " + this.nombre + ", comunmente conocida como " + this.nombreComun + " es " + getToStringComestible() + "? ¡Averigua más con mi aplicación de guía de Setas!";
+    }
+}
